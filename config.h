49012fb1fc1f4f803e3ab7a908fb5b38655a649b
danalexandru <danb.alexandru@gmail.com>
2022-04-02 12:35:38 +0300

    ignore: Test commit

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+}; /* stuff */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
