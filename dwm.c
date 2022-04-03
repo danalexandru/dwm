ee1f5783dc3ba98fedd3997368e0b59c02578083
danalexandru <danb.alexandru@gmail.com>
2022-04-03 18:34:41 +0300

    add: Added the rotatetags patch
    reference: https://dwm.suckless.org/patches/rotatetags/

--- dwm.c
+++ dwm.c
@@ -195,6 +195,7 @@ static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
+static void rotatetags(const Arg *arg);
 static void run(void);
 static void runAutostart(void);
 static void scan(void);
@@ -2239,3 +2240,30 @@ inplacerotate(const Arg *arg)
 	arrange(selmon);
 	focus(c);
 }
+
+void
+rotatetags(const Arg *arg)
+{
+	const int rot = abs(arg->i);
+	const unsigned int tagset = selmon->tagset[selmon->seltags];
+	unsigned int newtagset;
+
+	/* check the direction of the shift */
+	if (arg->i < 0) {
+		 /* shift tags right */
+		 newtagset = (tagset >> rot) | (tagset << (LENGTH(tags) - rot));
+	} else {
+		 /* shift tags left */
+		 newtagset = (tagset << rot) | (tagset >> (LENGTH(tags) - rot));
+	}
+
+	/* mask the tag bits */
+	newtagset &= TAGMASK;
+
+	if (newtagset) {
+		 selmon->tagset[selmon->seltags] = newtagset;
+		 focus(NULL);
+		 arrange(selmon);
+	}
+}
+
