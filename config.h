4c28f237858b07a398195ff73bcc2de17498f353
danalexandru <danb.alexandru@gmail.com>
2022-04-09 15:12:12 +0300

    update: Made dmenu colors mimic default dwm panel colors

--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray2, "-nf", col_cyan, "-sb", col_cyan, "-sf", col_gray2, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray2, "-nf", col_gray4, "-sb", col_gray2, "-sf", col_gray3, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
