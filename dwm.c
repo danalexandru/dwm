55a50519371114a38e35c0db73bf26724a01e631
danalexandru <danb.alexandru@gmail.com>
2022-04-22 20:15:17 +0300

    add: Added winicon patch
    reference: https://dwm.suckless.org/patches/winicon/

--- dwm.c
+++ dwm.c
@@ -241,6 +241,7 @@ static void togglescratch(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void togglewin(const Arg *arg);
+>>>>>>> 22bd112 (add: Added winicon patch)
 static void freeicon(Client *c);
 static void unfocus(Client *c, int setfocus);
 static void unmanage(Client *c, int destroyed);
@@ -772,7 +773,7 @@ drawbar(Monitor *m)
 
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon || 1) { /* status is drawn on all monitors */
-		drw_setscheme(drw, scheme[SchemeNorm]);
+		drw_setscheme(drw, scheme[SchemeStatus]);
         tw = TEXTW(stext);
         drw_text(drw, m->ww - tw, 0, tw, bh, lrpad / 2, stext, 0);
 	}
@@ -787,7 +788,7 @@ drawbar(Monitor *m)
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
 		w = TEXTW(tags[i]);
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeTagsSel : SchemeTagsNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
 			drw_rect(drw, x + boxw, 0, w - ( 2 * boxw + 1), boxw,
@@ -797,10 +798,10 @@ drawbar(Monitor *m)
 		x += w;
 	}
 	w = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeNorm]);
+	drw_setscheme(drw, scheme[SchemeTagsNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
-    if ((w = m->ww - tw - x) > bh) {
+	if ((w = m->ww - tw - x) > bh) {
 		if (n > 0) {
 			int remainder = (w - 2 * sp)% n;
 			int tabw = (1.0 / (double)n) * w + 1;
@@ -827,8 +828,10 @@ drawbar(Monitor *m)
 				x += tabw;
 			}
 		} else {
-			drw_setscheme(drw, scheme[SchemeNorm]);
-            drw_rect(drw, x, 0, w - 2 * sp, bh, 1, 1);
+			drw_setscheme(drw, scheme[SchemeInfoNorm]);
+            /* drw_rect(drw, x, 0, w - 2 * sp, bh, 1, 1); */
+            /* important: sp should be taken into consideration, but somehow this works better idk */
+            drw_rect(drw, x, 0, w, bh, 1, 1);
 		}
 	}
 
