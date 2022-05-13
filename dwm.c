edd57fa199589d439aa398e4566f081c1c863a73
danalexandru <danb.alexandru@gmail.com>
2022-05-13 19:12:06 +0300

    add: Added the "attachbottom" patch
    reference: https://dwm.suckless.org/patches/attachbottom/

--- dwm.c
+++ dwm.c
@@ -151,6 +151,7 @@ static int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interac
 static void arrange(Monitor *m);
 static void arrangemon(Monitor *m);
 static void attach(Client *c);
+static void attachbottom(Client *c);
 static void attachstack(Client *c);
 static void buttonpress(XEvent *e);
 static void checkotherwm(void);
@@ -421,6 +422,16 @@ attach(Client *c)
 	c->next = c->mon->clients;
 	c->mon->clients = c;
 }
+
+void
+attachbottom(Client *c)
+{
+	Client **tc;
+	c->next = NULL;
+	for (tc = &c->mon->clients; *tc; tc = &(*tc)->next);
+	*tc = c;
+}
+
 
 void
 attachstack(Client *c)
@@ -1146,7 +1157,7 @@ manage(Window w, XWindowAttributes *wa)
 		c->isfloating = c->oldstate = trans != None || c->isfixed;
 	if (c->isfloating)
 		XRaiseWindow(dpy, c->win);
-	attach(c);
+	attachbottom(c);
 	attachstack(c);
 	XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32, PropModeAppend,
 		(unsigned char *) &(c->win), 1);
@@ -1510,7 +1521,7 @@ sendmon(Client *c, Monitor *m)
 	detachstack(c);
 	c->mon = m;
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
-	attach(c);
+	attachbottom(c);
 	attachstack(c);
 	focus(NULL);
 	arrange(NULL);
@@ -2006,6 +2017,7 @@ updategeom(void)
 				m->mh = m->wh = unique[i].height;
 				updatebarpos(m);
 			}
+
 		/* removed monitors if n > nn */
 		for (i = nn; i < n; i++) {
 			for (m = mons; m && m->next; m = m->next);
@@ -2016,6 +2028,35 @@ updategeom(void)
 				c->mon = mons;
 				attach(c);
 				attachstack(c);
+
+	//		for (i = 0, m = mons; i < nn && m; m = m->next, i++)
+	//			if (i >= n
+	//			|| unique[i].x_org != m->mx || unique[i].y_org != m->my
+	//			|| unique[i].width != m->mw || unique[i].height != m->mh)
+	//			{
+	//				dirty = 1;
+	//				m->num = i;
+	//				m->mx = m->wx = unique[i].x_org;
+	//				m->my = m->wy = unique[i].y_org;
+	//				m->mw = m->ww = unique[i].width;
+	//				m->mh = m->wh = unique[i].height;
+	//				updatebarpos(m);
+	//			}
+	//	} else { /* less monitors available nn < n */
+	//		for (i = nn; i < n; i++) {
+	//			for (m = mons; m && m->next; m = m->next);
+	//			while ((c = m->clients)) {
+	//				dirty = 1;
+	//				m->clients = c->next;
+	//				detachstack(c);
+	//				c->mon = mons;
+	//				attachbottom(c);
+	//				attachstack(c);
+	//			}
+	//			if (m == selmon)
+	//				selmon = mons;
+	//			cleanupmon(m);
+
 			}
 			if (m == selmon)
 				selmon = mons;
