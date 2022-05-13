dea9b44c678277f9daec058e6285751bac98d787
danalexandru <danb.alexandru@gmail.com>
2022-05-13 19:16:18 +0300

    revert: Reverted the "attachbottom" patch

--- dwm.c
+++ dwm.c
@@ -1168,7 +1168,8 @@ manage(Window w, XWindowAttributes *wa)
 		c->isfloating = c->oldstate = trans != None || c->isfixed;
 	if (c->isfloating)
 		XRaiseWindow(dpy, c->win);
-	attachbottom(c);
+	// attachbottom(c);
+    attach(c);
 	attachstack(c);
 	XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32, PropModeAppend,
 		(unsigned char *) &(c->win), 1);
@@ -1534,7 +1535,8 @@ sendmon(Client *c, Monitor *m)
 	detachstack(c);
 	c->mon = m;
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
-	attachbottom(c);
+	// attachbottom(c);
+    attach(c);
 	attachstack(c);
 	focus(NULL);
 	arrange(NULL);
