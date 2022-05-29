cb455d33f3366e6ef65c018e084ce447448df7f8
danalexandru <danb.alexandru@gmail.com>
2022-05-29 11:11:09 +0300

    update: Changed the color of dmenu

--- config.h
+++ config.h
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static char dmenuprompt[] = "dmenu run:"; /* prompt to be displayed before input area */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray2, "-nf", col_gray4, "-sb", col_gray2, "-sf", col_gray3, "-p", dmenuprompt ,NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray2, "-nf", col_gray4, "-sb", col_gray4, "-sf", col_gray2, "-p", dmenuprompt ,NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
