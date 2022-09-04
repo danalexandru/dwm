a481f5abf670b2d8c2c997b40e25384c0218abf4
danalexandru <danb.alexandru@gmail.com>
2022-09-04 16:21:05 +0300

    delete: Removed "Hide" Scheme from all colorschemes (now Normal Scheme will be used instead)

--- colorschemes.h
+++ colorschemes.h
@@ -12,7 +12,6 @@ typedef struct {
 typedef struct {
     Scheme Normal;
     Scheme Select;
-    Scheme Hide;
 } ColorScheme;
 
 
@@ -29,11 +28,6 @@ const ColorScheme Default = {
         .Background = "#005577",
         .Foreground = "#eeeeee",
         .Border = "#005577",
-    },
-    .Hide = {
-        .Background = "#222222",
-        .Foreground = "#bbbbbb",
-        .Border = "#444444",
     }
 };
 
@@ -47,11 +41,6 @@ const ColorScheme DoomOne = {
         .Background = "#1F2833",
         .Foreground = "#F3F3F6",
         .Border = "#0087ff",
-    },
-    .Hide = {
-        .Background = "#14191F",
-        .Foreground = "#8C92AC",
-        .Border = "#14191F",
     }
 };
 
@@ -65,11 +54,6 @@ const ColorScheme MatchaDark = {
         .Background = "#263034",
         .Foreground = "#F3F3F6",
         .Border = "#16a085",
-    },
-    .Hide = {
-        .Background = "#141a1b",
-        .Foreground = "#16a085",
-        .Border = "#141a1b",
     }
 };
 
